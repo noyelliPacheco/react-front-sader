3bd91eb4256a2056c62bf515d081528cab3f09ca
Noyelli ITZEL <noye.itzel@gmail.com>
2026-01-12 14:29:23 -0600

    Decimaa subida

--- src/modulos/padron/empadronamiento/interfaces/catalogo.interface.ts
+++
@@ -1,7 +0,0 @@
-
-
-export interface Catalogo{
-    id: number;
-    nombre : string;
-    clave : string;
-}
--- src/modulos/padron/empadronamiento/interfaces/response/catalogos.response.ts
+++
@@ -1,9 +0,0 @@
-import type { Catalogo } from "./../catalogo.interface";
-
-export interface CatalogoResponse{
-    message: string;
-    success: boolean;
-    data:    Catalogo[] | [];
-    code:    number;
-}
-
